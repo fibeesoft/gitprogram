dcdf1d0ac9d2bd1410210c0b4dc83e5c2b306092
Mati <wilinski.mat@gmail.com>
2024-05-02 16:42:53 +0200

    my changes for pull request

--- gitprogram/Program.cs
+++ gitprogram/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello you again!");
+            Console.WriteLine("Hello you again from other account!");
         }
     }
 }
