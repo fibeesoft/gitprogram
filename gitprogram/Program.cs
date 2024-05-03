7dfa264eaa5d4e7ca78657f8731a0586c4e94c22
Matt <astraja.pro@gmail.com>
2024-05-03 08:37:51 +0200

    2nd feature

--- gitprogram/Program.cs
+++ gitprogram/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello you agaicn from aother account! as collaborator");
+            Console.WriteLine("Hello you agaicn 2nd feature");
         }
     }
 }
