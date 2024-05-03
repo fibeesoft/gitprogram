df779a176f7114b59322a81d813f05e1c7bdb7fe
Matt <astraja.pro@gmail.com>
2024-05-03 09:01:47 +0200

    ffff

--- gitprogram/Program.cs
+++ gitprogram/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello you agaicn fff");
+            Console.WriteLine("Hello you agaicn ffff");
         }
     }
 }
